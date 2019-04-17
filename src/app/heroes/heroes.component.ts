0b31bfd6af9993d20dd1d7d41441e88253c441b1
michael-basweti <baswetima@gmail.com>
2019-04-17 15:17:34 +0300

    redirect on clicking detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,30 +9,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // }
-
-  selectedHero: Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-
- 
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    this.getHeroes()
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes=>this.heroes=heroes)
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
